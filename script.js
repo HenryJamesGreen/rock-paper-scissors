e50e5bb9ab076f6e9b43a711b3303fd0f1f6c546
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-01-09 14:21:54 +0000

    added starter loop to eventually initiate choice of playing and extra loop for play again option

--- script.js
+++ script.js
@@ -1,55 +1,64 @@
-let userTell = prompt("Do you want to play a game?")
+let userTell = prompt("Do you want to play a game?");
 
-if (userTell === null) 
-    alert("You made me sad :'(");
+for (let i = 0; i < 2; i++) {
+
+
+userTell = userTell.toLowerCase();
+
+if (userTell === null || userTell === "no") { break; 
+  alert("You made me sad :'(");}
+
 
 if (userTell === "yes");
 
-userTell = userTell.toUpperCase();
-
 {
+  let computerChoices = ["r", "p", "s"];
 
+  let wins = 0;
+  let losses = 0;
+  let ties = 0;
 
-let computerChoices = ["r", "p", "s"];
+  while (wins + losses + ties < 5) {
+    let computerGuess =
+      computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-let wins = 0;
-let losses = 0;
-let ties = 0;
+    let userGuess = prompt("r, p, or s");
 
-while (wins + losses + ties < 5) {
-  let computerGuess =
-    computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    if (userGuess === null) {
+      alert("You made me sad :'(");
+    }
 
-  let userGuess = prompt("r, p, or s");
+    userGuess = userGuess.toLowerCase();
 
-  if (userGuess === null) {
-    alert("You made me sad :'(");
-  }
+    if (userGuess === "r" || userGuess === "p" || userGuess === "s") {
+      alert("The computer chose " + computerGuess);
 
-  userGuess = userGuess.toLowerCase();
-
-  if (userGuess === "r" || userGuess === "p" || userGuess === "s") {
-    alert("The computer chose " + computerGuess);
-
-    if (userGuess === computerGuess) {
-      ties++;
-      alert("You've tied " + ties + "time(s).");
-    } else if (
-      (userGuess === "r" && computerGuess === "p") ||
-      (userGuess === "p" && computerGuess === "s") ||
-      (userGuess === "s" && computerGuess === "r")
-    ) {
-      losses++;
-      alert("You lost " + losses + "time(s).");
-    } else {
-      wins++;
-      alert("You've won " + wins + "time(s).");
+      if (userGuess === computerGuess) {
+        ties++;
+        alert("You've tied " + ties + "time(s).");
+      } else if (
+        (userGuess === "r" && computerGuess === "p") ||
+        (userGuess === "p" && computerGuess === "s") ||
+        (userGuess === "s" && computerGuess === "r")
+      ) {
+        losses++;
+        alert("You lost " + losses + "time(s).");
+      } else {
+        wins++;
+        alert("You've won " + wins + "time(s).");
+      }
     }
   }
+
+  alert(
+    "Total wins: " +
+      wins +
+      "\nTotal losses: " +
+      losses +
+      "\nTotal ties: " +
+      ties
+  );
+
+  prompt("play again?");
 }
-
-alert(
-  "Total wins: " + wins + "\nTotal losses: " + losses + "\nTotal ties: " + ties
-);
-
 }
